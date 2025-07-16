e6b2e72aeb66ccb01933a410ca1e9325ff10e998
Pedro B Cavalcante <pedro.benevides@dtidigital.com.br>
2025-07-15 21:44:09 -0300

    feat: adiciona serviços de cache e worker no AssetBinding
    - Inclui a inicialização do PersistentCacheService e IsolateWorkerService.
    - Adiciona LRUCache para gerenciamento de cache de nós da árvore.
    - Atualiza o AssetController para utilizar os novos serviços.

--- lib/features/asset/asset_binding.dart
+++ lib/features/asset/asset_binding.dart
@@ -2,6 +2,11 @@ import 'package:get/get.dart';
 import 'package:tractian/features/asset/domain/usecases/get_company_assets_usecase.dart';
 import 'package:tractian/features/asset/domain/usecases/get_company_locations_usecase.dart';
 import 'package:tractian/features/asset/presentation/controllers/asset_controller.dart';
+import 'package:tractian/core/services/persistent_cache_service.dart';
+import 'package:tractian/core/services/isolate_worker_service.dart';
+import 'package:tractian/features/asset/core/utils/lru_cache.dart';
+import 'package:tractian/features/asset/domain/entities/tree_node.dart';
+import 'package:tractian/core/constants/constants.dart';
 
 class AssetBinding extends Bindings {
   @override
@@ -13,11 +18,20 @@ class AssetBinding extends Bindings {
       () => GetCompanyLocationsUseCaseImpl(Get.find()),
     );
 
+    Get.lazyPut<PersistentCacheService>(() => PersistentCacheService());
+    Get.lazyPut<IsolateWorkerService>(() => IsolateWorkerService());
+    Get.lazyPut<LRUCache<String, List<TreeNode>>>(
+      () => LRUCache<String, List<TreeNode>>(Constants.maxCacheSize),
+    );
+
     final companyId = Get.arguments['id'] as String;
     Get.lazyPut<AssetController>(
       () => AssetController(
         getCompanyLocations: Get.find(),
         getCompanyAssets: Get.find(),
+        persistentCache: Get.find(),
+        isolateWorker: Get.find(),
+        filterCache: Get.find(),
         companyId: companyId,
       ),
     );
